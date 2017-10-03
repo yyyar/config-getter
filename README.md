2744d13f21a5558979f8350510364787944a5fbb
Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
2017-10-03 14:34:54 +0300

    Refacoring; Added flexible configuration of fetchers; Added Consul fetcher

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 [![Build Status](https://travis-ci.org/yyyar/config-getter.svg?branch=master)](https://travis-ci.org/yyyar/config-getter) [![NPM version](https://badge.fury.io/js/config-getter.svg)](http://badge.fury.io/js/config-getter)
 
 Simple json configuration reader with file and env variables overrides.
+Can fetch / override variables using:
+- Local file
+- Env variables
+- Consul key-value
+
+For 0.0.x versions documentation see [older README.md](https://github.com/yyyar/config-getter/blob/v0.0.7/README.md)
 
 ### Installation
 ```bash
@@ -32,21 +38,66 @@ module.exports = {
 
 `app.js`
 ```javascript
-var getConfig = require('config-getter').getConfig;
+const getConfig = require('config-getter').getConfig;
 
-var config = getConfig(__dirname + '/default.js', {
-    /* optional parameters */
+//
+// Obtain final configuration object.
+//
+// Important Notice: even that underlying fetchers use async IO,
+// interface to getConfig is sync for usability purposes. 
+// As a result, Application that use config-getter should do getConfig once at startup time.
+//
+let config = getConfig(__dirname + '/default.js', {
 
-    // Name of env variables with path to extend default config file
-    configEnvVar: 'CONFIG_PATH',
+    /* ----- optional parameters ----- */
 
-    // Prefix for replacing config variables from env vars
-    configEnvPrefix: 'CONFIG_',
+    // if true, replaces arrays values
+    // If false, merge arrays
+    replaceArrays: false,
 
-    // if true, replaces arrays values, otherwise merge them
-    replaceArrays: false 
+    // if true, replaces objects values
+    // If false, merge objects
+    replaceObjects: false,
+
+    // If true, ignore fetcher exceptions and continue work
+    // If false, throw/propagate all exceptions
+    ignoreFailedFetchers: false,
+
+    // Optional fetchers
+    // Values from these fetchers will be merged into default config
+    // in order of their appear in the following array:
+    fetchers: [
+
+        /* ----- one or more of the following ----- */
+
+        {
+            type: 'consul',
+            opts: {
+                baseUrl: 'http://localhost:8500/v1',  // base url for Consul REST API
+                key: 'config'                         // key to look for JSON value
+            }
+        },
+
+        {
+            type: 'file',
+            opts: {
+                path: process.env.PATH_TO_CONFIG       // path to json file
+            }
+        },
+
+        {
+            type: 'env',
+            opts: {
+                prefix: 'CONFIG_',                    // prefix of env variable used to override values
+                ignore: '^PATH_TO_CONFIG$'            // regexp to ignore env variable if there is a match
+            }
+        }
+    ]
+
 });
+
 console.log(config);
+
 ```
 
 Simple config loading
@@ -56,12 +107,12 @@ $ node app.js
 
 Override config with overrides.json values
 ```bash
-$ CONFIG_PATH=./overrides.json node app.js
+$ PATH_TO_CONFIG=./overrides.json node app.js
 ```
 
 Override config with overrides.json values and single key value
 ```bash
-$ CONFIG_PATH=./overrides.json CONFIG_db_port=7777 node app.js
+$ PATH_TO_CONFIG=./overrides.json CONFIG_db_port=7777 node app.js
 ```
 
 ### Placeholders
--- tests/simple.js
+++ tests/simple.js
@@ -3,61 +3,149 @@
  * @author Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
  */
 
-var _ = require('lodash'),
-    getConfig = require('../lib').getConfig;
+const _ = require('lodash'),
+      getConfig = require('../lib').getConfig;
 
-var configsDir = __dirname + '/configs';
+const configsDir = __dirname + '/configs';
 
 /* exports */
 module.exports = {
 
+    setUp: function(callback) {
+        process.env = _.dropWhile(process.env, (o) => { return o.startsWith('CG_'); });
+        callback();
+    },
+
     'Simple config': function(test) {
-        var config = getConfig(configsDir + '/default.js');
+        let config = getConfig(configsDir + '/default.js');
         test.equal(config.hello, 'world', "Key exsits in config");
         test.done();
     },
 
     'Config file overrides': function(test) {
-        process.env.CONFIG_PATH = configsDir + '/override.json';
 
-        var config = getConfig(configsDir + '/default.js');
+        let config = getConfig(configsDir + '/default.js', {
+            fetchers: [
+                {
+                    type: 'file',
+                    opts: {
+                        path: configsDir + '/override.json'
+                    }
+                }
+            ]
+        });
+
         test.equal(config.hello, 'overrided', "Key is overrided in config");
         test.done();
     },
 
     'Config file and env overrides': function(test) {
-        process.env.CONFIG_PATH = configsDir + '/override.json';
-        process.env.CONFIG_another_stuff = "cool";
 
-        var config = getConfig(configsDir + '/default.js');
+        process.env.CG_ENV_another_stuff = 'cool';
+
+        let config = getConfig(configsDir + '/default.js', {
+            fetchers: [
+                {
+                    type: 'file',
+                    opts: {
+                        path: configsDir + '/override.json'
+                    }
+                },
+                {
+                    type: 'env',
+                    opts: {
+                        prefix: 'CG_ENV_'
+                    }
+                }
+            ]
+        });
+
         test.equal(config.hello, 'overrided', "Key overrided in config");
         test.equal(config.another.stuff, 'cool', "Key added in config from env variable");
         test.done();
     },
 
     'Placeholders work': function(test) {
-        process.env.CONFIG_PATH = configsDir + '/placeholders.json';
 
-        var config = getConfig(configsDir + '/default.js');
+        let config = getConfig(configsDir + '/default.js', {
+            fetchers: [
+                {
+                    type: 'file',
+                    opts: {
+                        path: configsDir + '/placeholders.json'
+                    }
+                }
+            ]
+        });
+
         test.equal(config.phrase, config.some.object, "Placeholder works");
         test.equal(config.phraseDuplicated, config.some.object + config.some.object, "Duplicaterd placeholder works");
         test.done();
     },
 
     'Relative placeholders': function(test) {
-        process.env.CONFIG_PATH = null;
-          var config = getConfig(configsDir + '/relativePlaceholders.json');
+        let config = getConfig(configsDir + '/relativePlaceholders.json');
         test.equal(config.guys.person.phrase, "Hello, " + config.guys.person.name + " and " + config.guys.buddy.name,
                 "Placeholder works");
         test.done();
     },
 
-    'Array replacenent': function(test) {
-        process.env.CONFIG_PATH = configsDir + '/arrayOverride.json';
+    'Array replacement': function(test) {
 
-        var config = getConfig(configsDir + '/array.json', {replaceArrays: true});
+        let config = getConfig(configsDir + '/array.json', {
+            replaceArrays: true,
+            fetchers: [
+                {
+                    type: 'file',
+                    opts: {
+                        path: configsDir + '/arrayOverride.json'
+                    }
+                }
+            ]
+        });
         test.deepEqual(config.array, require(configsDir+'/arrayOverride').array, "Array is replaced in config");
         test.done();
     },
 
+    'Broken override file throws exception': function(test) {
+
+        test.throws(() => {
+            let x = getConfig(configsDir + '/default.js', {
+                fetchers: [
+                    {
+                        type: 'file',
+                        opts: {
+                            path: configsDir + '/broken.json'
+                        }
+                    }
+                ]
+            });
+        }, Error, 'Throws exception');
+
+        test.done();
+    },
+
+    /*
+    'Consul': function(test) {
+
+        let config = getConfig(configsDir + '/default.js', {
+            replaceArrays: true,
+            fetchers: [
+                {
+                    type: 'consul',
+                    opts: {
+                        baseUrl: 'http://localhost:8500/v1',
+                        key: 'test/config'
+                    }
+                }
+            ]
+        });
+
+        test.equal(config.consul, 'fromconsul', "Value from consul presented");
+
+        test.done();
+    }
+    */
+
 };
+
